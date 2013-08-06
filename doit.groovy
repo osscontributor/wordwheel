b8835d3928d008361476262a3dfb357098eb9ed5
Jeff Scott Brown <jbrown@gopivotal.com>
2013-08-06 08:48:56 -0500

    optimize

--- doit.groovy
+++ doit.groovy
@@ -1,15 +1,17 @@
 
-(2..6).each { wordSize ->
+(9..12).each { wordSize ->
 words = [] as Set
-
+prefixes = [] as Set
 new File('words.txt').eachLine { line ->
     line = line.trim().toUpperCase()
     if(line?.size() == wordSize) {
         words << line
+        (1..(wordSize-2)).each {
+            prefixes << line[0..it]
+        }
     }
 }
 letters = ('A'..'Z')
-
 
 // A = 65
 // Z = 90
@@ -36,20 +38,46 @@ getWords = { offsets ->
         println foundWords
     }
 }
-
-rigIt = null
-rigIt = { coll ->
-    if(coll.size() < wordSize - 1) {
-        26.times { t ->
-            rigIt(coll + t)
+getWord = { column, offsets ->
+    word = "" + getChar(column)
+    offsets.each { offset ->
+        word += getChar(column + offset)
+    }
+    word
+}
+processOffsets = null
+processOffsets = { offsets ->
+    if(offsets.size() == wordSize -2 ) {
+        for(int newOffset = 0; newOffset < 26; newOffset++) {
+            newOffsets = offsets + newOffset
+            foundWords = [] as SortedSet
+            for(int column = 0; column < 26; column++) {
+                word = getWord(column, newOffsets)
+                if(words.contains(word)) {
+                    foundWords << word
+                }
+            }
+            if(foundWords.size() > 1) {
+                wordSets << foundWords
+            }
         }
-    } else {
-        getWords coll
+    } else if(offsets.size < wordSize -2) {
+        for(int newOffset = 0; newOffset < 26; newOffset++) {
+            matchCount = 0
+            newOffsets = offsets + newOffset
+            for(int column = 0; column < 26 && matchCount < 2; column++) {
+                if(prefixes.contains(getWord(column, newOffsets))) {
+                    matchCount++
+                }
+            }
+            if(matchCount > 1) {
+                println newOffsets
+                processOffsets newOffsets
+            }
+        }
     }
 }
-foo = []
-rigIt foo
-
+processOffsets ([])
 
 new File("results_${wordSize}.txt").withPrintWriter { writer ->
 wordSets.sort { it[0] }.sort { it.size() }.eachWithIndex { words, idx ->

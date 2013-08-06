d4c3e05179d491a480044519d183435b66398356
Jeff Scott Brown <jbrown@gopivotal.com>
2013-08-05 21:15:48 -0500

    initial

---
+++ doit.groovy
@@ -0,0 +1,37 @@
+words = []
+new File('words.txt').eachLine { line ->
+    line = line.trim()
+    if(line) {
+        words << line
+    }
+}
+letters = ('A'..'Z')
+
+
+// A = 65
+// Z = 90
+getChar = { int offset ->
+    c = 65 + offset
+    if(c > 90) c -= 26
+    (char)c
+}
+
+getWords = { off1, off2, off3 ->
+    foundWords = []
+    (0..25).each { p1 ->
+        word = "" + getChar(p1) + getChar(off1 + p1) + getChar(off2 + p1) + getChar(off3 + p1)
+        if(words.contains(word)) {
+            foundWords << word
+        }
+    }
+    if(foundWords.size() > 1) {
+        println foundWords
+    }
+}
+(0..25).each { off1 ->
+   (0..25).each { off2 ->
+       (0..25).each { off3 ->
+           getWords(off1, off2, off3)
+       }
+   }
+}
